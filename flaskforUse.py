a653eac475d078f63f8d7f98ea3833c4c434b430
ellie_chen <cool30132@gmail.com>
2024-02-07 11:12:41 +0800

    change dataShown function location

--- flaskforUse.py
+++ flaskforUse.py
@@ -36,86 +36,21 @@ def query_artist(name):
 
 @app.route("/album/<string:name>")
 def query_ablum(name):
-    if name:
-        try:
-            # columns=['Album_Name', 'Type', 'Total_tracks','Release_date', 'Artists', 'Available_markets','External_urls']
-            columns=['Album_cover', 'Album_Name', 'Type', 'Release_date', 'Total_tracks', 'Artists','External_urls']
-
-            # search album info using artist name and ID, for specific
-            name1 = 'rapper_album_information'
-            name2 = 'rapper_information'
-            table1 = Spotify(name1)
-            table2 = Spotify(name2)
-
-            artistID = table2.get_collection_search_query('name', name)['id']
-            query = {'items.artists.name': name, 'items.artists.id': artistID}
-            df = pd.DataFrame(table1.get_collection_search_query_multiple_conditions(query, 'and'))
-            if not df.empty:
-                templist1 = []
-                for i in df['items']:
-                    for j in i:
-                        artisList = []
-                        for idx, x in enumerate(j['artists']):
-                            if name in x.values():
-                                len1 = len(j['artists'])
-                                for y in range(len1):
-                                    artisList.append(j['artists'][y]['name'])
-                                templist1.append({columns[0]: j['images'][0]['url'], 
-                                                columns[1]: j['name'], 
-                                                columns[2]: j['album_type'], 
-                                                columns[3]: j['release_date'], 
-                                                columns[4]: j['total_tracks'], 
-                                                columns[5]: artisList, 
-                                                columns[6]: j['external_urls']['spotify']})
-            
-            dfshow = pd.DataFrame.from_dict(templist1)
-
-            # remove duplicated data and sort by release date in descending order
-            dfshow['Release_date'] = pd.to_datetime(dfshow['Release_date'], format='%Y%m%d', errors='ignore')
-            dfshow = dfshow.loc[dfshow.astype(str).drop_duplicates().index].sort_values(by='Release_date', ascending=False).reset_index(drop=True)
-            dfDict = dfshow.to_dict('records')
-
-            return render_template('index2v2.html', artistName = name, table_data = dfDict, colname = columns)
-            # return render_template('index2.html', tables=[dfshow.to_html(classes='data', header="false")])
-
-        except:
-            return 'No album information found!'
+    res = dataAnalysis.ablumData(name)
+    if res == 0:
+        return 'No album information found!'
+    else:
+        return render_template('index2v2.html', artistName = res[0], table_data = res[1], colname = res[2])
         
 
 @app.route('/tracks/<string:artName>/<string:albName>')  # fisrt one is artist's name, second one is album name
-def trackinoGen(artName, albName):
-    try:
-        collName1 = 'rapper_album_information'
-        collName2 = 'rapper_track_general_information'
-        table1 = Spotify(collName1)
-        table2 = Spotify(collName2)
-
-        query1 = {'items.name': albName, 'items.artists.name': artName}
-        df1 = pd.DataFrame(table1.get_collection_search_query_multiple_conditions(query1, 'and'))
-        list1 = df1.loc[0]['items']
-        df2 = pd.DataFrame.from_records(list1)
-        albumID = df2[df2['name'] == albName]['id'].iloc[0]
-
-
-        '''columns to retrieve'''
-        cols = ['Track Name', 'Artists', 'External_urls']
-        query2 = {"$regex":f"{albumID}"}
-        res = table2.get_collection_search_query('href', query2)
-        templist = []
-        for i in res['items']:
-            singerlist = []
-            for j in i['artists']:
-                singerlist.append(j['name'])
-            templist.append({cols[0]: i['name'],
-                            cols[1]: singerlist,
-                            cols[2]: i['external_urls']['spotify']
-                            })
-
-        return render_template('trackInfo1.html', albumName = albName, albumID = albumID, table_data = templist, colname = cols, artName = artName)
-
-    except:
-        return 'No track information yet!'
-    
+def trackinfoGen(artName, albName):
+        res = dataAnalysis.trackDataGen(artName, albName)
+        if res == 0:
+            return 'No track information yet!'
+        else:
+            return render_template('trackInfo1.html', albumName = res[0], albumID = res[1], table_data = res[2], colname = res[3], artName = res[4])
+        
 
 @app.route("/tracks/detail/<string:albumID>")
 def trackDetail(albumID):
